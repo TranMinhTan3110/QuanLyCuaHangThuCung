4be0a41e4e49624530bfa988641316e2347352ea
Vanhung0611 <vanhungvn741@gmail.com>
2025-04-08 09:31:55 +0700

    update petView

---
+++ src/view/testPanel.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.JFrame;
+
+
+public class testPanel {
+	public static void main(String[] args) {
+        JFrame frame = new JFrame("Manage Users");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(950, 750);
+        frame.add(new ProductView());
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
