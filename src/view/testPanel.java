fdb0127010fe3f9e762f105e7f1a6ac0d398b7d8
Vanhung0611 <vanhungvn741@gmail.com>
2025-05-26 11:15:42 +0700

    Update testPanel.java

--- src/view/testPanel.java
+++ src/view/testPanel.java
@@ -1,14 +1,16 @@
 package view;
-
 import javax.swing.JFrame;
 
+import view.BillView;
+
+
 public class testPanel {
-	public static void DatabaseConnectionin(String[] args) {
-		JFrame frame = new JFrame("DatabaseConnectionnage Users");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(950, 750);
-		frame.add(new CustomerView());
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
+	public static void main(String[] args) {
+        JFrame frame = new JFrame("Manage Users");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(950, 750);
+        frame.add(new BillView());
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
